ce5a9c91a582e83c41105b1dae6e39948d803697
Yousef <theyousefkhan@gmail.com>
2023-12-06 23:42:36 -0800

    Day 7

---
+++ make_dirs.py
@@ -0,0 +1,11 @@
+import os
+
+os.makedirs('Day 08')
+open('Day 08/day8input.txt', 'w').close()
+open('Day 08/day8test.txt', 'w').close()
+
+os.makedirs('Day 08/Part 1')
+os.makedirs('Day 08/Part 2')
+
+open('Day 08/Part 1/day8.py', 'w').close()
+open('Day 08/Part 2/day8.py', 'w').close()
